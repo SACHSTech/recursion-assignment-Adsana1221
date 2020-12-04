aae04a14a103618bb9791879011953994eb03a07
Adsana Mohendran <adsana.mohendran21@ycdsbk12.ca>
2020-12-04 15:48:08 +0000

    wrote code for test 2

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -4,17 +4,18 @@ public class Recursion{
 
 
 public static int count7(int n){
-
+  
   // base case
-  // if the number is less than 7 =, there won't be an count for the number 7
+  // if the number is less than 7, there won't be a count for the number 7
   if (n < 7){
     return 0;
   }
-    //else if (n % 10 == 7){
-    //  return 1;
-    
+    else if (n % 10 == 7){
+      return 1;
+    }
     // }else{ 
-       return 0 + count7(n / 10);
+       return 1 + count7(n / 10);
+       
 
 }
 }
