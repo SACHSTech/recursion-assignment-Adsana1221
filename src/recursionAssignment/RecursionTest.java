dc579036faa7769864d63e1cc907a05687f17a71
Adsana Mohendran <adsana.mohendran21@ycdsbk12.ca>
2020-12-04 00:24:48 +0000

    first fail test

--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -1,17 +1,13 @@
 package recursionAssignment;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Before;
-import org.junit.jupiter.api.After;
+//import org.junit.jupiter.api.Before;
+//import org.junit.jupiter.api.After;
 
 import recursionAssignment.Recursion;
 
 public class RecursionTest{
     
-    @Before
-    public void beforeTest(){
-      // some steps to perform before tests are run
-    }
     
     /**
      * An initial test
@@ -19,16 +15,12 @@ public class RecursionTest{
     @Test
     public void Test1(){
         // make assertion statement(s)
-        // assertEquals(EXPECTEDVALUE, TESTVALUE);
+     assertEquals(0, Recursion.count7("123"));
         // assertTrue(TESTBOOLEAN);
     }
     
     // add more tests
     
-    @After
-    public void afterTest(){
-      // some steps to perform after tests are run
-    }
-    
+   
     
 }
