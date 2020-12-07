9332b16138816f4fd5d9bdada39a13671b402087
Adsana Mohendran <adsana.mohendran21@ycdsbk12.ca>
2020-12-07 00:28:00 +0000

    added comments for test 3

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -11,10 +11,10 @@ public static int count7(int n){
     return 0;
   }
     // if the right most number is 7, return a count of 1
-    // 
+    // if not 7, take the last digits out and try again with the remaining digits
     else if (n % 10 == 7){
-      return 1 + count7(n / 10)
-;    }
+      return 1 + count7(n / 10);
+    }
 
      else{
       return count7(n / 10);
